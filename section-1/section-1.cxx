8c012e15d61118bc78c06170260969e7fc60d061
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-03-24 19:15:18 +0700

    finish exercise 10

--- section-1/section-1.cxx
+++ section-1/section-1.cxx
@@ -1,7 +1,7 @@
 int exercise001(int n) {
     int s = 0;
 
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i < n + 1; i++) {
         s = s + i;
     }
     return s;
@@ -9,7 +9,7 @@ int exercise001(int n) {
 
 int exercise002(int n) {
     unsigned long long sum = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n + 1; i++) {
         sum = sum + i * i;
     }
     return sum;
@@ -17,7 +17,7 @@ int exercise002(int n) {
 
 double exercise003(int n) {
     double sum = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i < n + 1; i++) {
         sum = sum + 1.0 / i;
     }
     return sum;
@@ -25,23 +25,67 @@ double exercise003(int n) {
 
 double exercise004(int n) {
     double sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum = sum + 1.0 / (2.0 * i)
+    for (int i = 1; i < n + 1; i++) {
+        sum = sum + 1.0 / (2.0 * i);
     }
     return sum;
 }
 
-int exercise005(int n);
+double exercise005(int n) {
+    double sum = 0;
+    for (int i = 0; i < n + 1; i++) {
+        sum = sum + 1.0 / (2 * i + 1);
+    }
+    return sum;
+}
 
-int exercise006(int n);
+double exercise006(int n) {
+    double sum;
+    for (int i = 0; i < n + 1; i++) {
+        sum = sum + 1.0 / (i * (i + 1));
+    }
+    return sum;
+}
 
-int exercise007(int n);
+double exercise007(int n) {
+    double sum;
+    for (float i = 1; i < n + 1; i++) {
+        sum = sum + i / (i + 1);
+    }
 
-int exercise008(int n);
+    return sum;
+}
 
-int exercise009(int n);
+double exercise008(int n) {
+    double sum = 0;
+    for (float i = 1; i < n + 1; i++) {
+        sum = sum + (2 * i + 1) / (2 * i + 2);
+    }
 
-int exercise010(int n);
+    return sum;
+}
+
+unsigned long long exercise009(int n) {
+    unsigned long long product = 1;
+    for (int i = 0; i < n + 1; i++) {
+        product = product * i;
+    }
+    return product;
+
+}
+
+unsigned long long exercise010(int x, int n) {
+    int base = x;
+    int exponent = n;
+//    in this case, we consider to calculate natural number power
+//    For real number implementation, checkout internet
+    unsigned long long answer = base;
+    for (int i = 1; i < exponent+1; ++i) {
+        answer = answer*base;
+    }
+    return answer;
+}
+
 
 int exercise011(int n);
 
