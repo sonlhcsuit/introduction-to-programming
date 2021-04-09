8a9cc8e8a6fbec261181178272ffcd6e9f97a386
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-04-09 18:40:48 +0700

    ex4

--- section-1/section-1.cxx
+++ section-1/section-1.cxx
@@ -1,8 +1,64 @@
 int exercise001(int n) {
     int s = 0;
 
-    for(int i=0;i<=n;i++){
-        s=s+i;
+    for (int i = 0; i <= n; i++) {
+        s = s + i;
     }
     return s;
 }
+
+int exercise002(int n) {
+    unsigned long long sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i * i;
+    }
+    return sum;
+}
+
+double exercise003(int n) {
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + 1.0 / i;
+    }
+    return sum;
+}
+
+double exercise004(int n) {
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + 1.0 / (2.0 * i)
+    }
+    return sum;
+}
+
+int exercise005(int n);
+
+int exercise006(int n);
+
+int exercise007(int n);
+
+int exercise008(int n);
+
+int exercise009(int n);
+
+int exercise010(int n);
+
+int exercise011(int n);
+
+int exercise012(int n);
+
+int exercise013(int n);
+
+int exercise014(int n);
+
+int exercise015(int n);
+
+int exercise016(int n);
+
+int exercise017(int n);
+
+int exercise018(int n);
+
+int exercise019(int n);
+
+int exercise020(int n);
--- section-1/section-1.h
+++ section-1/section-1.h
@@ -7,4 +7,24 @@
 
 #endif //INTRODUCTION_TO_PROGRAMMING_EX1_H
 
+
 int exercise001(int n);
+int exercise002(int n);
+double exercise003(int n);
+double exercise004(int n);
+int exercise005(int n);
+int exercise006(int n);
+int exercise007(int n);
+int exercise008(int n);
+int exercise009(int n);
+int exercise010(int n);
+int exercise011(int n);
+int exercise012(int n);
+int exercise013(int n);
+int exercise014(int n);
+int exercise015(int n);
+int exercise016(int n);
+int exercise017(int n);
+int exercise018(int n);
+int exercise019(int n);
+int exercise020(int n);
