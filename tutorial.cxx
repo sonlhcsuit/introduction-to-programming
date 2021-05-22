965408d23f74d5b5b08b17674a2b61cddff665b8
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-22 10:49:08 +0700

    remove CMake file, because to annoy for understand the whole cmake

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,29 +1,6 @@
-// A simple program that computes the square root of a number
-#ifdef USE_SECTION_ONE
-#include "SectionOne.h"
-#endif
-
-
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
-#include <string>
-
-#include "Introduction-to-programming-config.h"
-
-int main(int argc, char* argv[])
-{
-  if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
-  const double inputValue = std::stod(argv[1]);
-
-  // convert input to double
-  #ifdef USE_SECTION_ONE
-    const double outputValue = mysqrt(inputValue);
-  #else
-    const double outputValue = sqrt(inputValue);
-  #endif
-  return 0;
+using namespace std;
+int main(){
+  cout << "Hello World\n";
+  return 1;
 }
