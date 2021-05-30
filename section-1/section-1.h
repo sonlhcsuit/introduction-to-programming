3c9eddc08cd981f8d7e48b6050f43a3f4166fda8
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-30 11:28:49 +0700

    update setting up

--- section-1/section-1.h
+++ section-1/section-1.h
@@ -133,17 +133,20 @@ bool exercise060(long long n);
 
 bool exercise061(long long n);
 
-int exercise062(int a,int b);
+int exercise062(int a, int b);
 
-int exercise063(int a,int b);
+int exercise063(int a, int b);
 
-double exercise064(double a, double b );
+double exercise064(double a, double b);
 
-vector<double> exercise065(double a, double b ,double c);
+vector<double> exercise065(double a, double b, double c);
 
-vector<double> exercise066(double a, double b ,double c);
+vector<double> exercise066(double a, double b, double c);
 
 double exercise067(int n, double x);
+
 double exercise068(int n, double x);
+
 double exercise069(int n, double x);
+
 double exercise070(int n, double x);
