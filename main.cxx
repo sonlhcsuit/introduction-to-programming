3979da1e331dff31ad68c126fb98630f6ea8d558
Lưu Hoàng Sơn <sonlh.cs.uit@gmail.com>
2021-08-03 20:48:51 +0700

    update miscs

--- main.cxx
+++ main.cxx
@@ -1,21 +1,20 @@
-#include <iostream>
-#include <string>
-#include <cmath>
 #include "section-1/section-1.h"
 #include "section-2/section-2.h"
 #include "section-5/section-5.h"
+#include <cmath>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    cout << "Hello World\n";
+  cout << "Hello World\n";
 
-    vector<float> v = {4444.0, 2.0, 8.0};
-    vector<int> y = {3, 9, 27, 81, 90, 100,0};
+  vector<float> v = {4444.0, 2.0, 8.0};
+  vector<int> y = {3, 9, 27, 81, 90, 100, 0};
 
-    bool t = exercise240(y);
-    cout << t;
+  bool t = exercise240(y);
+  cout << t;
 
-
-    return 1;
+  return 1;
 }
